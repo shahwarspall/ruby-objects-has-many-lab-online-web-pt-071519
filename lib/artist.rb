b877a3ad9e34cfef1a6340d5f771dbefa03ecaa9
Shahwar Spall <shevy.spall@gmail.com>
2019-08-14 05:04:08 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,34 +1,34 @@
-class Artist 
-  
-  attr_accessor :name  
-  @@all = []
- 
+
+class Artist
+
+  attr_accessor :name, :artist, :genre
+
+  ALL=[]
+
   def initialize(name)
     @name = name
-     @@all << self
-   
-  end 
-  
- 
-  
-  
+    ALL << self
+  end
+
   def self.all
-    @@all
+    ALL
   end
-  
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    self.songs.last
+  end
+
   def songs
-    Song.all.select {|song| song.artist == self}
+    Song.all.select do |song|
+      song.artist == self
+    end
   end
-  
-  def add_song(song)
-    song.artist = self
-  end 
-  
-  def add_song_by_name(name, genre)
-    song = Song.new(name, genre)
-  
-  end  
-  
-  
-  
+
+  def genres
+    Song.all.map do |song|
+      song.genre
+    end
+  end
+
 end
